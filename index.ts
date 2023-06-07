ca86942a3243953049dbd9c14ed3164f18d37ef6
kibrukuture <kibrukuture@gmail.com>
2023-06-07 21:30:29 +0300

    modified:   bun.lockb 	modified:   index.ts 	modified:   package.json

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
- 
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  // send a text message
+
+  res.status(200).send("WELCOME TO BUN SERVER, ADDING A NEW ONE!");
+});
+
+app.listen(Bun.env.PORT, () => {
+  console.log(`Listening at port ${"http://localhost"}:${Bun.env.PORT}`);
+});
